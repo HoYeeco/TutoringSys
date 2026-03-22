50f7c88f65353aa658ca4a6d6f438b498ebc2141
HoYeeco <hoyeeco@outlook.com>
2026-03-22 15:44:06 +0800

    删除冗余结构

--- backend/tutoringsys-api/src/main/java/com/tutoringsys/service/TeacherStatsService.java
+++
@@ -1,9 +0,0 @@
-package com.tutoringsys.service;
-
-import com.tutoringsys.api.dto.AssignmentStatsVo;
-import com.tutoringsys.api.dto.CourseStatsVo;
-
-public interface TeacherStatsService {
-    AssignmentStatsVo getAssignmentStats(Long assignmentId);
-    CourseStatsVo getCourseStats(Long courseId);
-}
