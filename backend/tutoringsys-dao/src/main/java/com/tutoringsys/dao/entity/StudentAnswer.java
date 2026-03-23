0cac57de8ecf930df008c0a3bc40db3ff415f164
HoYeeco <hoyeeco@outlook.com>
2026-03-23 11:13:43 +0800

    答题与批改管理

--- backend/tutoringsys-dao/src/main/java/com/tutoringsys/dao/entity/StudentAnswer.java
+++ backend/tutoringsys-dao/src/main/java/com/tutoringsys/dao/entity/StudentAnswer.java
@@ -1,7 +1,6 @@
 package com.tutoringsys.dao.entity;
 
 import com.baomidou.mybatisplus.annotation.TableName;
-import com.baomidou.mybatisplus.annotation.Version;
 import lombok.Data;
 
 import java.util.Date;
@@ -14,23 +13,9 @@ public class StudentAnswer {
     private Long studentId;
     private Long assignmentId;
     private Long questionId;
-    private String answer;
     private String answerContent;
-    private String feedback;
-    private String teacherFeedback;
-    private String aiFeedback;
-    private Integer isGraded;
     private Integer score;
-    private Integer aiScore;
-    private Integer finalScore;
-    private Integer status;
-    private Integer reviewStatus;
-    private Long reviewerId;
+    private String feedback;
     private String graderType;
-    private Date submitTime;
-    private Date reviewTime;
     private Date createTime;
-    private Date updateTime;
-    @Version
-    private Integer version;
 }
