3da3c68e08ca63b316807b6f6592930b91afb161
HoYeeco <hoyeeco@outlook.com>
2026-04-01 00:47:09 +0800

    Create QuestionDetailDTO.java

---
+++ backend/src/main/java/com/tutoring/dto/QuestionDetailDTO.java
@@ -0,0 +1,26 @@
+package com.tutoring.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class QuestionDetailDTO {
+
+    private Long id;
+    private String type;
+    private String content;
+    private List<String> options;
+    private String answer;
+    private String referenceAnswer;
+    private Integer score;
+    private Integer minWords;
+    private Integer maxWords;
+    private Integer sortOrder;
+}
